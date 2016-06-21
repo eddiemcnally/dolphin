3759b9c7f428fab75c5a1fdd944df8d87475d691
eddiemcnally <emcn@gmx.com>
2016-06-21 19:34:47 +0100

    more files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod board;
 mod piece;
+mod util;
+mod occupancy_masks;
 
 fn main() {
 #![allow(dead_code)]
@@ -7,6 +9,13 @@ fn main() {
 
     println!("pawn piece value {}",
              piece::get_value(piece::Piece::WQueen));
+
+
+    let x: u64 = util::set_bit(0, 5);
+    println!("x = {}", x);
+
+    let y:u64 = occupancy_masks::get_occupancy_mask(piece::Piece::WQueen, board::Square::d1);
+    println!("occ mask {}", y);
 
 
 }
--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,8 @@
 
-fn set_bit(brd: &u64, bit: u8) {
-    *brd = *brd | (u64) (0x01 << bit);
+pub fn set_bit(brd: u64, bit: u8) -> u64 {
+    brd | (0x01 << bit)
 }
 
-fn clear_bit(brd: &u64, bit: u8){
-     *brd = *brd & (u64) (~(0x01 << bit));
+pub fn clear_bit(brd: u64, bit: u8) -> u64 {
+    brd & (!(0x01 << bit))
 }
