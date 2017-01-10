e3f160569904e986a18b67f0223da6bc3136fac7
eddiemcnally <emcn@gmx.com>
2017-01-10 19:41:22 +0000

    work-in-progress on FEN parsing

--- src/board.rs
+++ src/board.rs
@@ -15,26 +15,37 @@ pub enum CastlePermission {
 }
 
 
-
+// TODO: look into decomposing this into a set of sub-structs and Impl's
+// to improve manageability
 #[allow(dead_code)]
 pub struct Board {
+    // bitboard representing occupied/vacant squares (for all pieces)
     board_bb: BitBoard,
+    // piece bitboard, an entry for each piece type (enum Piece)
     piece_bb: [BitBoard; piece::NUM_PIECES],
+    // bitboard for each Colour
     colour_bb: [BitBoard; piece::NUM_COLOURS],
+    // side to move
     side_to_move: piece::Colour,
+    // the en passant square
     en_pass_sq: Square,
+    // castle permissions
     castle_perm: u8,
 }
 
 
 impl Board {
-    pub fn add_piece(&self, pce: piece::Piece, sq: Square) {
+    pub fn add_piece(&self) {
         // *self.board_bb.set_bit(sq);
         // *self.piece_bb[pce as u64].set_bit(sq);
         // *self.colour_bb[pce.colour() as u8].set_bit(sq);
     }
 
-    pub fn remove_piece(&self, pce: piece::Piece) {}
+    pub fn remove_piece(&self) {}
+
+    pub fn set_piece_to_move(&mut self, col: piece::Colour) {
+        self.side_to_move = col;
+    }
 }
 
 
--- src/fen.rs
+++ src/fen.rs
@@ -4,48 +4,84 @@ use square::Rank;
 use square::File;
 
 
+/// parses a FEN string and populates the given board
+///
+/// Sample FEN:
+///      rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
+///
+pub fn get_position(fen: &str, board: &mut board::Board){
+    let v: Vec<&str> = fen.split(' ').collect();
+
+    // [0] = piece positions
+    // [1] = piece to move
+    // [2] = castle permissions
+    // [3] = en passant square (or '-' if no en passant)
+    // [4] = half-move clock
+    // [5] = full move number
 
 
-pub fn construct_position(fen: &str, board: &mut board::Board){
+
+
+
+
+}
+
+/// takes the list of ranks (starting at rank 8)
+populate_piece_positions(rank_list Vec<&str>, board: &mut board::Board){
+
+    let ranks = vec![Rank::Rank8, Rank::Rank7, Rank::Rank6, Rank::Rank5, Rank::Rank4, Rank::Rank3, Rank::Rank2, Rank::Rank1];
+
+
+
+
+}
+
+
+
+
+pub fn aaa__construct_position(fen: &str, board: &mut board::Board){
 
     let mut rank = Rank::Rank8;
     let mut file = File::FileA;
     for c in fen.chars(){
         let mut pce_to_add:piece::Piece;
         let mut count = 0;
+        let pce = try_get_piece(&c);
         match c{
-            'p' => pce_to_add = piece::Piece::BPawn,
-            'P' => pce_to_add = piece::Piece::WPawn,
-            'r' => pce_to_add = piece::Piece::BRook,
-            'R' => pce_to_add = piece::Piece::WRook,
-            'b' => pce_to_add = piece::Piece::BBishop,
-            'B' => pce_to_add = piece::Piece::WBishop,
-            'n' => pce_to_add = piece::Piece::BKnight,
-            'N' => pce_to_add = piece::Piece::WKnight,
-            'q' => pce_to_add = piece::Piece::BQueen,
-            'Q' => pce_to_add = piece::Piece::WQueen,
-            'k' => pce_to_add = piece::Piece::BKing,
-            'K' => pce_to_add = piece::Piece::BPawn,
-            '1' => count => 1,
-            '2' => count => 2,
-            '3' => count => 3,
-            '4' => count => 4,
-            '5' => count => 5,
-            '6' => count => 6,
-            '7' => count => 7,
-            '8' => count => 8,
-            '/' | ' ' =>{
-                rank = rank.decr();
-                file = File::FileA;
-            },
+            Some(),
+            None(){
+                '1' => count => 1,
+                '2' => count => 2,
+                '3' => count => 3,
+                '4' => count => 4,
+                '5' => count => 5,
+                '6' => count => 6,
+                '7' => count => 7,
+                '8' => count => 8,
+                '/' | ' ' =>{
+                    rank = rank.decr();
+                    file = File::FileA;
+                },
+            }
             _ => panic()
         }
-
-
-
-
     }
 
-
-
+    fn gtry_get_piece(fen_char &str) -> Option<piece::Piece>{
+        match *fen_char {
+            'p' => pce_to_add = Some(piece::Piece::BPawn),
+            'P' => pce_to_add = Some(piece::Piece::WPawn),
+            'r' => pce_to_add = Some(piece::Piece::BRook),
+            'R' => pce_to_add = Some(piece::Piece::WRook),
+            'b' => pce_to_add = Some(piece::Piece::BBishop),
+            'B' => pce_to_add = Some(piece::Piece::WBishop),
+            'n' => pce_to_add = Some(piece::Piece::BKnight),
+            'N' => pce_to_add = Some(piece::Piece::WKnight),
+            'q' => pce_to_add = Some(piece::Piece::BQueen),
+            'Q' => pce_to_add = Some(piece::Piece::WQueen),
+            'k' => pce_to_add = Some(piece::Piece::BKing),
+            'K' => pce_to_add = Some(piece::Piece::BPawn),
+            _ => None
+        }
+    }
 }
