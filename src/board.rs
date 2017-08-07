5ad54bf212d2dae3955967ebf30f4d73a2531312
eddiemcnally <emcn@gmx.com>
2017-08-07 19:36:11 +0100

    Started fleshing out the board.rs module

--- src/board.rs
+++ src/board.rs
@@ -2,8 +2,13 @@
 #[allow(non_camel_case_types)]
 
 use bitboard::BitBoard;
-use piece;
+use piece::Piece;
+use piece::NUM_PIECES;
+use piece::NUM_COLOURS;
+use square::Square;
+use std::option::Option;
 
+pub const NUM_SQUARES: usize = 64;
 
 // TODO: look into decomposing this into a set of sub-structs and Impl's
 // to improve manageability
@@ -12,27 +17,26 @@ pub struct Board {
     // bitboard representing occupied/vacant squares (for all pieces)
     board_bb: BitBoard,
     // piece bitboard, an entry for each piece type (enum Piece)
-    piece_bb: [BitBoard; piece::NUM_PIECES],
+    piece_bb: [BitBoard; NUM_PIECES],
     // bitboard for each Colour
-    colour_bb: [BitBoard; piece::NUM_COLOURS],
+    colour_bb: [BitBoard; NUM_COLOURS],
+    // the pieces on each square
+    pieces: [Piece; NUM_SQUARES],
 }
 
 
 impl Board {
-    pub fn add_piece(&self) {
+    pub fn add_piece(&self, piece: Piece, sq: Square) {
         // *self.board_bb.set_bit(sq);
         // *self.piece_bb[pce as u64].set_bit(sq);
         // *self.colour_bb[pce.colour() as u8].set_bit(sq);
     }
 
-    pub fn remove_piece(&self) {}
+    pub fn remove_piece(&self, piece: Piece, sq: Square) {}
+
+    pub fn move_piece(&self, from_sq: Square, to_sq: Square, piece: Piece) {}
+
+    pub fn get_piece_on_square(&self, sq: Square) -> Option<Piece> {
+        return None;
+    }
 }
-
-
-
-
-
-
-
-#[allow(dead_code)]
-pub const NUM_SQUARES: usize = 64;
