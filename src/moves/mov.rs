42d6fcb862bb8fe6da9875f748fdb1bbb1eeb1e6
eddiemcnally <emcn@gmx.com>
2018-11-18 19:27:20 +0000

    More cleanup of mov. Added docunmentation.

--- src/moves/mov.rs
+++ src/moves/mov.rs
@@ -4,31 +4,11 @@ use board::square::Square;
 use std::ops::Shl;
 use std::ops::Shr;
 
-// bitmap for type Move
-// See http://chessprogramming.wikispaces.com/Encoding+Moves
-//
-//  ---- ---- --11 1111      To Square
-//  ---- 1111 11-- ----      From Square
-//  0000 ---- ---- ----      Quiet move
-//  0001 ---- ---- ----      Double Pawn push
-//  0010 ---- ---- ----      King Castle
-//  0011 ---- ---- ----      Queen Castle
-//  0100 ---- ---- ----      Capture
-//  0101 ---- ---- ----      En Passant Capture
-//  1000 ---- ---- ----      Promotion Knight
-//  1001 ---- ---- ----      Promotion Bishop
-//  1010 ---- ---- ----      Promotion Rook
-//  1011 ---- ---- ----      Promotion Queen
-//  1100 ---- ---- ----      Promotion Knight Capture
-//  1101 ---- ---- ----      Promotion Bishop Capture
-//  1110 ---- ---- ----      Promotion Rook Capture
-//  1111 ---- ---- ----      Promotion Queen Capture
-
 //#[derive(Eq, PartialEq, Hash, Debug, Clone, Copy)]
 pub struct Mov(u16);
 
 impl Mov {
-    /// Encodes a Quite move given the "from" and "to" squares
+    /// Encodes a Quiet move given the "from" and "to" squares
     ///
     /// # Arguments
     ///
@@ -110,50 +90,79 @@ impl Mov {
         mov
     }
 
+    /// Encodes a Double Pawn first move
+    ///
+    /// # Arguments
+    ///
+    /// * `from_sq`         - the from square
+    /// * `to_sq`           - the to square
+    ///
     pub fn encode_move_double_pawn_first(from_sq: &Square, to_sq: &Square) -> Mov {
         let mut mov = Mov::encode_quiet(from_sq, to_sq);
         mov.0 = mov.0 | MV_FLG_DOUBLE_PAWN;
         mov
     }
 
+    /// Encodes a White King-side castle move
+    ///
     pub fn encode_move_castle_kingside_white() -> Mov {
+        // todo: this can be determined at compile time, so fix this
         let mut mov = Mov::encode_quiet(&Square::e1, &Square::g1);
         mov.0 = mov.0 | MV_FLG_KING_CASTLE;
         mov
     }
 
+    /// Encodes a Black King-side castle move
+    ///
     pub fn encode_move_castle_kingside_black() -> Mov {
         let mut mov = Mov::encode_quiet(&Square::e8, &Square::g8);
         mov.0 = mov.0 | MV_FLG_KING_CASTLE;
         mov
     }
 
+    /// Encodes a White Queen-side castle move
+    ///
     pub fn encode_move_castle_queenside_white() -> Mov {
         let mut mov = Mov::encode_quiet(&Square::e1, &Square::c1);
         mov.0 = mov.0 | MV_FLG_QUEEN_CASTLE;
         mov
     }
 
+    /// Encodes a Black Queen-side castle move
+    ///
     pub fn encode_move_castle_queenside_black() -> Mov {
         let mut mov = Mov::encode_quiet(&Square::e8, &Square::c8);
         mov.0 = mov.0 | MV_FLG_QUEEN_CASTLE;
         mov
     }
 
-    pub fn is_equal(mv1: &Mov, mv2: &Mov) -> bool {
-        mv1.0 == mv2.0
-    }
-
+    /// Encodes a White Queen-side castle move
+    ///
     pub fn decode_from_square(mv: &Mov) -> Square {
         let sq = (mv.0 & MV_MASK_FROM_SQ).shr(MV_SHFT_FROM_SQ);
         Square::from_u8(sq as u8)
     }
 
+    ///
+    /// Decodes the "to" square from the Move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn decode_to_square(mv: &Mov) -> Square {
         let sq = (mv.0 & MV_MASK_TO_SQ).shr(MV_SHFT_TO_SQ);
         Square::from_u8(sq as u8)
     }
 
+    ///
+    /// Decodes the promotion piece from the move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    /// * `side`       - the side/colour
+    ///
     pub fn decode_promotion_piece(mv: &Mov, side: &Colour) -> Piece {
         let masked = mv.0 & MV_MASK_FLAGS;
 
@@ -175,39 +184,107 @@ impl Mov {
         }
     }
 
+    /// Tests the given move to see if it is a Quiet move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_quiet(mv: &Mov) -> bool {
         let m = mv.0 & MV_MASK_FLAGS;
         m == MV_FLG_QUIET
     }
 
+    /// Tests the given move to see if it is a Capture move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_capture(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_BIT_CAPTURE) != 0
     }
 
+    /// Tests the given move to see if it is a Promotion move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_promote(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_BIT_PROMOTE) != 0
     }
 
+    /// Tests the given move to see if it is an En Passant move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_en_passant(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_EN_PASS) != 0
     }
 
+    /// Tests the given move to see if it is a Castle move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_castle(mv: &Mov) -> bool {
         Mov::is_king_castle(mv) || Mov::is_queen_castle(mv)
     }
 
+    /// Tests the given move to see if it is an Queen-side castle move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_queen_castle(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_QUEEN_CASTLE) != 0
     }
 
+    /// Tests the given move to see if it is an King-side castle move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_king_castle(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_KING_CASTLE) != 0
     }
 
+    /// Tests the given move to see if it is a Double pawn first move
+    ///
+    /// # Arguments
+    ///
+    /// * `mv`         - the move to decode
+    ///
     pub fn is_double_pawn(mv: &Mov) -> bool {
         (mv.0 & MV_FLG_DOUBLE_PAWN) != 0
     }
 }
+
+// bitmap for type Move
+// See http://chessprogramming.wikispaces.com/Encoding+Moves
+//
+//  ---- ---- --11 1111      To Square
+//  ---- 1111 11-- ----      From Square
+//  0000 ---- ---- ----      Quiet move
+//  0001 ---- ---- ----      Double Pawn push
+//  0010 ---- ---- ----      King Castle
+//  0011 ---- ---- ----      Queen Castle
+//  0100 ---- ---- ----      Capture
+//  0101 ---- ---- ----      En Passant Capture
+//  1000 ---- ---- ----      Promotion Knight
+//  1001 ---- ---- ----      Promotion Bishop
+//  1010 ---- ---- ----      Promotion Rook
+//  1011 ---- ---- ----      Promotion Queen
+//  1100 ---- ---- ----      Promotion Knight Capture
+//  1101 ---- ---- ----      Promotion Bishop Capture
+//  1110 ---- ---- ----      Promotion Rook Capture
+//  1111 ---- ---- ----      Promotion Queen Capture
 
 const MV_FLG_QUIET: u16 = 0x0000;
 const MV_FLG_DOUBLE_PAWN: u16 = 0x1000;
@@ -233,3 +310,6 @@ const MV_SHFT_FROM_SQ: u16 = 6;
 const MV_MASK_TO_SQ: u16 = 0x003F;
 const MV_MASK_FROM_SQ: u16 = 0x0FC0;
 const MV_MASK_FLAGS: u16 = 0xF000;
+
+#[cfg(test)]
+pub mod tests {}
