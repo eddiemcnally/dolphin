5920e6cc5e18f4111717e6350450ee2bd39a337e
eddiemcnally <emcn@gmx.com>
2020-02-22 11:18:34 +0000

    Added some perf notes as a prelude to optimisation.

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,25 @@ mod utils;
 use input::fen;
 use perft::perft_runner;
 use position::position::Position;
+use std::time::Instant;
 
 fn main() {
-    let epd_rows = perft::epd_parser::extract_epd("../../src/resources/perftsuite.epd".to_string());
+    // let epd_rows = perft::epd_parser::extract_epd("../../src/resources/perftsuite.epd".to_string());
 
-    for epd in &epd_rows {
-        println!("Testing FEN '{}'", epd.fen);
+    // for epd in &epd_rows {
+    //     println!("Testing FEN '{}'", epd.fen);
 
-        for depth in 1..7 {
-            process_row(epd, depth);
-        }
-    }
+    //     for depth in 1..7 {
+    //         process_row(epd, depth);
+    //     }
+    // }
 
-    // let epd = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ;D1 48 ;D2 2039 ;D3 97862 ;D4 4085603 ;D5 193690690 ;D6 8031647685";
-    // let row = perft::epd_parser::extract_row(epd.to_string());
-    // process_row(&row, 3);
+    let epd = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ;D1 48 ;D2 2039 ;D3 97862 ;D4 4085603 ;D5 193690690 ;D6 8031647685";
+    let depth = 5;
+    println!("Sampl Perf : Depth: {}, perft - {}", depth, epd);
+
+    let row = perft::epd_parser::extract_row(epd.to_string());
+    process_row(&row, depth);
 }
 
 fn process_row(row: &perft::epd_parser::EpdRow, depth: u8) {
@@ -40,9 +44,11 @@ fn process_row(row: &perft::epd_parser::EpdRow, depth: u8) {
     let parsed_fen = fen::get_position(&fen);
     let mut position = Position::new(parsed_fen);
 
-    //println!("{}", position.board());
-
+    let now = Instant::now();
     let num_moves = perft_runner::perft(depth, &mut position);
+    let elapsed_in_secs = now.elapsed().as_secs_f64();
+
+    println!("Nodes/sec: {}", num_moves as f64 / elapsed_in_secs);
 
     if *expected_moves != num_moves {
         println!(
